8002fbdc84cd3f3baede492273fe0cd8f02953bd
Frédéric NOËL <contact@fredericnoel.com>
2019-03-18 10:01:32 +0100

    Classe Animal() Classe qui permet d'instancier des animals.

---
+++ classes/Animal.php
@@ -0,0 +1,119 @@
+<?php
+
+class Animal
+{
+    private $masse;
+    private $embranchement;
+    private $classe;
+    private $nbrePattes;
+    private $sexe;
+    private $metabolisme;//Cycle normal, reproduction, hibernation
+
+    public function seReproduire()
+    {
+        return "Reproduction";
+    }
+
+    public function besoinsEnergetiques()
+    {
+        return $this->metabolisme;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMasse()
+    {
+        return $this->masse;
+    }
+
+    /**
+     * @param mixed $masse
+     */
+    public function setMasse($masse): void
+    {
+        $this->masse = $masse;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmbranchement()
+    {
+        return $this->embranchement;
+    }
+
+    /**
+     * @param mixed $embranchement
+     */
+    public function setEmbranchement($embranchement): void
+    {
+        $this->embranchement = $embranchement;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getClasse()
+    {
+        return $this->classe;
+    }
+
+    /**
+     * @param mixed $classe
+     */
+    public function setClasse($classe): void
+    {
+        $this->classe = $classe;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNbrePattes()
+    {
+        return $this->nbrePattes;
+    }
+
+    /**
+     * @param mixed $nbrePattes
+     */
+    public function setNbrePattes($nbrePattes): void
+    {
+        $this->nbrePattes = $nbrePattes;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSexe()
+    {
+        return $this->sexe;
+    }
+
+    /**
+     * @param mixed $sexe
+     */
+    public function setSexe($sexe): void
+    {
+        $this->sexe = $sexe;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMetabolisme()
+    {
+        return $this->metabolisme;
+    }
+
+    /**
+     * @param mixed $metabolisme
+     */
+    public function setMetabolisme($metabolisme): void
+    {
+        $this->metabolisme = $metabolisme;
+    }
+
+
+}
