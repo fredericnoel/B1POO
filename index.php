a8545e8b13f8349978f700dea3960f70961359d3
Frédéric NOËL <contact@fredericnoel.com>
2019-03-18 14:02:40 +0100

    Autoloader. Tests divers.

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
-require_once './classes/Animal.php';
 
-$chat = new Animal();
+date_default_timezone_set('Europe/Paris');
+require_once './functions/classAutoLoader.php';
+spl_autoload_register('classAutoLoader');
 
-var_dump($chat);
+$chat = new Felin(3);
+
+$michel = new Citoyen();
+$michel->setAdresse(42, "rue de la République", "76210", "Bolbec");
+$michel->allerALaPlage();
+
+//Log::logWrite(serialize($chat));
